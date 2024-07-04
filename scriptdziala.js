df5e9fee98d622d41f25bca8ec3d4dc704ebca8e
jslusarczykk <j.slusarczykk@gmail.com>
2024-07-04 23:42:46 +0200

    cos

---
+++ scriptdziala.js
@@ -0,0 +1,5 @@
+function toggleBackgroundGif() {
+    const bodyElement = document.body;
+    bodyElement.classList.toggle("backgroundGifOn");
+    console.log(bodyElement.classList); // Log the classList
+}
